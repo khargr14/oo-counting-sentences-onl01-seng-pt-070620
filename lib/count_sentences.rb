4bbdbebb744a6815e54ba5d5d7b305f851d5d8e1
Keesha Hargrove <kharg14@gmail.com>
2020-07-23 23:10:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -13,7 +13,7 @@ class String
   end
   
   def count_sentences
-    self.split(/[.!?]/).reject {|x| x.empty?}.size
+    self.split(/[.!?]/).reject {|x| x.empty?}.siz
   end
   
   def count_sentences_words
